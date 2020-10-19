3ba8efb9c96c554c5180c668f6e4bee3d482db1c
Tayler <Tae@myfreeapps.com>
2020-10-19 16:31:11 -0600

    finish up basic dice rolling and turn taking mechanics

--- diceroll/diceroll.js
+++ diceroll/diceroll.js
@@ -7,15 +7,32 @@ let dicePicEl = document.getElementById("dicePic")
 dicePicEl.addEventListener("click", roll)
 let playerOneEl = document.getElementById("playerOne")
 let playerTwoEl = document.getElementById("playerTwo")
+let playerOneScoreEl = document.getElementById("playerOneRoll")
+let playerTwoScoreEl = document.getElementById("playerTwoRoll")
+
+// let continueButtonEl = document.getElementById("button")
+// continueButtonEl.addEventListener("click", instructions)
+// let messageOneEl = document.getElementById("messageOne")
+
+
+// let partOfInstructions = 0;
 
 
 playerOneEl.innerHTML = playerOne
 playerTwoEl.innerHTML = playerTwo
 // console.log(playerOne)
 
-let rollOne = 0
-let rollTwo = 0
-let totalRoll = 0
+
+
+
+
+// function instructions(){
+//     if(partOfInstructions === 0){
+//         messageOneEl.classList.remove("remove")
+//     }else if(partOfInstructions === 1){
+
+//     }
+// }
 
 
 function getRandomInt(max) {
@@ -23,9 +40,39 @@ function getRandomInt(max) {
     (max));
 }
 
+//dice roll
+
+let rollOne = 0
+let rollTwo = 0
+let totalRoll = 0
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerTurn = 1;
+
 function roll(){
-    rollOne = getRandomInt(7)+1
-    rollTwo = getRandomInt(7)+1
-    totalRoll = rollOne + rollTwo
-    console.log(`you rolled a ${rollOne} and a ${rollTwo} which adds to ${totalRoll}`)
+        if(playerTurn === 1){
+            // console.log(`it is player ${playerTurn}'s turn`)
+            rollOne = getRandomInt(7)+1
+            rollTwo = getRandomInt(7)+1
+            totalRoll = rollOne + rollTwo
+            playerOneScore = playerOneScore + totalRoll
+            playerOneScoreEl.innerHTML = playerOneScore
+            playerTurn = 2
+            console.log(`player 1 rolled a ${rollOne} and a ${rollTwo} which adds to ${totalRoll}`)
+            console.log(`Player 1 now has the score of ${playerOneScore}`)
+            console.log(`it is now player ${playerTurn}'s turn`)
+
+            
+        }else if(playerTurn === 2){
+            // console.log(`it is player ${playerTurn}'s turn`)
+            rollOne = getRandomInt(7)+1
+            rollTwo = getRandomInt(7)+1
+            totalRoll = rollOne + rollTwo
+            playerTwoScore = playerTwoScore + totalRoll
+            playerTwoScoreEl.innerHTML = playerTwoScore
+            playerTurn = 1
+            console.log(`player 2 rolled a ${rollOne} and a ${rollTwo} which adds to ${totalRoll}`)
+            console.log(`Player 2 now has the score of ${playerTwoScore}`)
+            console.log(`it is now player ${playerTurn}'s turn`)
+        }
 }
