e770c9843ab568cff195b9b4ab3d22de07e5b475
stegmanta <tae@myfreeapps.com>
2020-11-02 09:22:27 -0700

    started drawing at schhol

---
+++ drawing/drawing.js
@@ -0,0 +1,48 @@
+console.log("yoyoyoyo")
+// let x = 0;
+
+function setup(){
+    let red = color(`#FF0000`)
+    let orange = color(`#FF4500`)
+    createCanvas(600,600)
+    fill(0)
+}
+
+function draw(){
+    background(100)
+}
+
+
+function rainbow(){
+    for(let x = 0; x <= 100; x++){
+
+    }
+}
+
+
+
+//----animation----
+// function setup(){
+//     createCanvas(400,400)
+//     fill(0)
+//     frameRate(100)
+//     createLoop({duration:10, gif:false})
+// }
+
+// function draw() {
+//     background(255)
+//     translate(width / 2, height / 2)
+//     const radius = height / 3
+//     const x = cos(animLoop.theta) * radius
+//     const y = sin(animLoop.theta) * radius
+//     ellipse(x, y, 50, 50)
+
+//     for(let x = 0;x <= 100; x++){
+//         ellipse(x,y,5,5)
+//     }
+
+// }
+
+
+
+//--end of animation--
